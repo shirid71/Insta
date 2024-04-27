3eccb33e988fdc65290ce1fb31c779797ef77e2a
shiri.hameiri <shiri.hameiri@servicenow.com>
2024-04-27 16:21:24 +0300

    step2

--- src/cmps/ImgUploader.jsx
+++ src/cmps/ImgUploader.jsx
@@ -26,7 +26,7 @@ export function ImgUploader({ onUploaded = null }) {
     <div className="upload-preview">
       {imgData.imgUrl && <img src={imgData.imgUrl} style={{ maxWidth: '200px', float: 'right' }} />}
       <label htmlFor="imgUpload">{getUploadLabel()}</label>
-      <input type="file" onChange={uploadImg} accept="img/*" id="imgUpload" />
+      <input type="file" onChange={uploadImg} accept="image/*" id="imgUpload" />
     </div>
   )
 }
